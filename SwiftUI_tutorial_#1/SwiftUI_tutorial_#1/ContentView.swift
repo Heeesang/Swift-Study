0d415cacb4b6c265d086bb84711960382ffd9174
Heesang <ghs050611@naver.com>
2022-03-07 20:53:47 +0900

    swift

---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by 곽희상 on 2022/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/SwiftUI_tutorial__1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorial__1App.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by 곽희상 on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorial__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
