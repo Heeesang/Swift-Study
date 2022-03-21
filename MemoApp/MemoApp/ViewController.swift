4d4e7f5e4dae6072a2d3511af795f92b230a311b
Heeesang <ghs050611@naver.com>
2022-03-21 22:54:15 +0900

    [Create] MemoApp project

---
+++ MemoApp/MemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 곽희상 on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
