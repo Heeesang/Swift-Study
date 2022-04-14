c4ddbd6a4af040a40eb2224b101ecd91c4f7876c
Heeesang <ghs050611@naver.com>
2022-04-14 09:29:00 +0900

    URLImage

---
+++ RandomuserAPI/RandomuserAPI/RandomuserAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomuserAPIApp.swift
+//  RandomuserAPI
+//
+//  Created by 곽희상 on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct RandomuserAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomuserAPI/RandomuserAPI/UI/ProfileImgView.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileImg.swift
+//  RandomuserAPI
+//
+//  Created by 곽희상 on 2022/04/14.
+//
+ 
+import SwiftUI
+import URLImage
+
+struct ProfileImgView: View {
+    var imageurl: URL
+    
+    var body: some View {
+        URLImage(imageurl) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.green, lineWidth: 2))
+        
+    }
+}
+
+struct ProfileImgView_Previews: PreviewProvider {
+    static var previews: some View {
+        let url = URL(string: "https://randomuser.me/api/portraits/men/55.jpg")!
+        
+        ProfileImgView(imageurl: url)
+    }
+}
