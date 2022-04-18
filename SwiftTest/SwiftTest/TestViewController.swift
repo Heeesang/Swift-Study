ce856402ddff75bffb19e4758b4834ee9f731bee
Heeesang <ghs050611@naver.com>
2022-04-18 18:22:18 +0900

    Alamofire 통신 with dev.준화

--- SwiftTest/SwiftTest/TestViewController.swift
+++ SwiftTest/SwiftTest/TestViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 import Alamofire
 
 class TestViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    
+
     let myTableView: UITableView = UITableView()
-    let items: [String] = ["곽희상", "swift", "ios"]
-    
+    var dataSource: [RandomUser] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchFilms()
+
         view.addSubview(myTableView)
         myTableView.delegate = self
         myTableView.dataSource = self
@@ -29,32 +29,39 @@ class TestViewController: UIViewController, UITableViewDelegate, UITableViewData
             myTableView.rightAnchor.constraint(equalTo:view.rightAnchor),
             myTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor)
         ])
+    }
+    
+    func fetchFilms() {
         
+        let urlString = "https://randomuser.me/api/?results=3"
+        AF.request(urlString).responseJSON { (response) in
+                    switch response.result {
+                    case .success(let res):
+                        do {
+                            let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
+                            let json = try JSONDecoder().decode(RandomUserResponse.self, from: jsonData)
+                            self.dataSource = json.results
+                            DispatchQueue.main.async {
+                                self.myTableView.reloadData()
+                            }
+                        } catch(let err) {
+                            print(err.localizedDescription)
+                        }
+                    case .failure(let err):
+                        print(err.localizedDescription)
+                    }
+                }
     }
 }
 
 extension TestViewController{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return dataSource.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = myTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = dataSource[indexPath.row].name.title
         return cell
     }
-    
-    func fetchFilms() {
-        
-        let urlString = "https://randomuser.me/api/?results=1"
-        
-        AF
-            .request(urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
-            .responseDecodable(of: RandomUserResponse.self) {response in
-                guard case .success(let data) = response.result else {return}
-                print(data.results)
-            }
-      }
 }
 
