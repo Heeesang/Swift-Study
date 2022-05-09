31e182829975cb3dd5b85c4ba74742de33b8bcea
Heeesang <ghs050611@naver.com>
2022-05-09 23:20:49 +0900

    firebase With Dev.Jun

--- Oasis/Oasis/App/AppDelegate.swift
+++ Oasis/Oasis/App/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:              [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
