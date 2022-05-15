fc396c758b9f7112612b3a1095891361ae938978
Heeesang <ghs050611@naver.com>
2022-05-15 18:20:37 +0900

    Setting

---
+++ MVVMPractice/MVVMPractice/MainVC.swift
@@ -0,0 +1,14 @@
+//
+//  MainVC.swift
+//  MVVMPractice
+//
+//  Created by 곽희상 on 2022/05/15.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+    
+    
+}
---
+++ MVVMPractice/MVVMPractice/Models/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  MVVMPractice
+//
+//  Created by 곽희상 on 2022/05/15.
+//
+
+import Foundation
